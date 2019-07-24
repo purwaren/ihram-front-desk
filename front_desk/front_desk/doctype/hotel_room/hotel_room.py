699dfd769515b550274d879dbb08453233551291
Fadhlan Hidayat <mail.fadhlanh@gmail.com>
2019-07-24 16:19:38 +0700

    update hotel room. view photos

--- front_desk/front_desk/doctype/hotel_room/hotel_room.py
+++ front_desk/front_desk/doctype/hotel_room/hotel_room.py
@@ -8,3 +8,7 @@ from frappe.website.website_generator import WebsiteGenerator
 
 class HotelRoom(WebsiteGenerator):
 	pass
+
+@frappe.whitelist()
+def get_images(room_type):
+		return frappe.db.get_list('File', filters={'attached_to_doctype':'Room Type', 'attached_to_name':room_type}, fields=['file_url'])
