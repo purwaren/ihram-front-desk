430baaf808114639e09db24d7b5193e1f09d4b39
Fadhlan Hidayat <mail.fadhlanh@gmail.com>
2019-07-31 17:37:08 +0700

    update deposit payment method

--- front_desk/front_desk/doctype/room_stay/room_stay.js
+++ front_desk/front_desk/doctype/room_stay/room_stay.js
@@ -1,12 +1,24 @@
 // Copyright (c) 2019, PMM and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Room Stay', {
-	setup: function(frm, cdt, cdn) {
-		frm.set_df_property('deposit', 'set_only_once', 1);
+var wrapper = null;
+var room_stay = null;
+var has_deposit = false;
 
-		var room_stay = frappe.get_doc(cdt, cdn);
-		if (room_stay.deposit.length <= 0){
+frappe.ui.form.on('Room Stay', {
+	setup: function(frm, cdt, cdn){
+
+	},
+	before_load: function(frm, cdt, cdn) {
+		wrapper = frm.get_field("payment_method").$wrapper;
+		frm.set_df_property('deposit', 'set_only_once', 1);
+		room_stay = frappe.get_doc(cdt, cdn);
+		if (room_stay.deposit.length > 0) {
+			has_deposit = true;
+		}
+	},
+	onload: function(frm, cdt, cdn) {
+		if (!has_deposit) {
 			frappe.call({
 				method: 'front_desk.front_desk.doctype.folio.folio.get_folio_name',
 				args: {
@@ -22,7 +34,18 @@ frappe.ui.form.on('Room Stay', {
 					frm.refresh_field('deposit');
 				}
 			});
+		}
 
+		// frm.fields_dict['deposit'].grid.get_field('folio_id').get_query = function () {
+		// 	return {
+		// 		filters: {
+		// 			'reservation_id': room_stay.reservation_id
+		// 		}
+		// 	}
+		// }
+	},
+	refresh: function(frm, cdt, cdn) {
+		if (!has_deposit) {
 			var df = null;
 
 			df = frappe.meta.get_docfield('Folio Transaction', 'folio_id', room_stay.name);
@@ -36,20 +59,24 @@ frappe.ui.form.on('Room Stay', {
 
 			df = frappe.meta.get_docfield('Folio Transaction', 'remark', room_stay.name);
 			df.read_only = 1;
-		}
 
-		// frm.fields_dict['deposit'].grid.get_field('folio_id').get_query = function () {
-		// 	return {
-		// 		filters: {
-		// 			'reservation_id': room_stay.reservation_id
-		// 		}
-		// 	}
-		// }
+			var html = '<select id="payment_method"><option value="Cash">Cash</option><option value="Debit">Debit</option></select>';
+			wrapper.html(html);
+		}
 	},
-	onload: function(frm) {
+	onload_post_render: function(frm, cdt, cdn) {
 		
 	},
-	refresh: function(frm) {
+	before_save: function(frm, cdt, cdn) {
 		
+	},
+	after_save: function(frm, cdt, cdn) {
+		if (!has_deposit){
+			var e = document.getElementById("payment_method");
+			var v = e.options[e.selectedIndex].value;
+
+			wrapper.html(null);
+			has_deposit = true;
+		}
 	}
 });
