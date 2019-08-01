f926243e6445d1e1697eb27aab11c1274638d787
anra54 <anugrah.ramadhani@gmail.com>
2019-08-01 17:43:42 +0700

    checkout, fungsi update reservation status menjadi finish

--- front_desk/front_desk/doctype/reservation/reservation.py
+++ front_desk/front_desk/doctype/reservation/reservation.py
@@ -3,6 +3,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import json
 import frappe
 from frappe.model.document import Document
 from front_desk.front_desk.doctype.room_stay.room_stay import create_room_stay
@@ -15,3 +16,19 @@ class Reservation(Document):
 def check_in(reservation_id_list):
 	create_folio(reservation_id_list)
 	return create_room_stay(reservation_id_list)
+
+@frappe.whitelist()
+def check_out(reservation_id_list):
+	# TODO: Calculate trx and print receipt
+
+	# Update reservation status to "FINISH"
+	reservation_id_list = json.loads(reservation_id_list)
+
+	for reservation_id in reservation_id_list:
+		reservation = frappe.get_doc('Reservation', reservation_id)
+		reservation.status = "Finish"
+		reservation.save()
+
+	# TODO: Update room_status dari hotel_room menjadi "Vacant Dirty"
+	
+	# TODO: Update status dari hotel_room menjadi "OO"
