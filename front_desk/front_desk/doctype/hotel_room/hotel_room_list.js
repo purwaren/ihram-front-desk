008d690af31f84a3c9903334ae374437b5e4674a
Fadhlan Hidayat <mail.fadhlanh@gmail.com>
2019-08-23 13:37:06 +0700

    add confirmation update status hotel room

--- front_desk/front_desk/doctype/hotel_room/hotel_room_list.js
+++ front_desk/front_desk/doctype/hotel_room/hotel_room_list.js
@@ -2,15 +2,20 @@ frappe.listview_settings['Hotel Room'] = {
     onload: function(listview) {
         if (frappe.user.has_role('Housekeeping') || frappe.user.has_role('Housekeeping Supervisor')) {
             listview.page.add_action_item(__('Update Status'), function() {
-                frappe.call({
-                    method: "front_desk.front_desk.doctype.hotel_room.hotel_room.upgrade_status",
-                    args: {
-                        room_name_list: listview.get_checked_items(true)
-                    },
-                    callback: (response) => {
-                        
+                frappe.confirm(
+                    ('You are about to update status Hotel Rooms ' + listview.get_checked_items(true) + ', are you sure?'),
+                    () => {
+                        frappe.call({
+                            method: "front_desk.front_desk.doctype.hotel_room.hotel_room.upgrade_status",
+                            args: {
+                                room_name_list: listview.get_checked_items(true)
+                            },
+                            callback: (response) => {
+                                frappe.msgprint(("Hotel Rooms ") + listview.get_checked_items(true) + (" status updated."));
+                            }
+                        });
                     }
-                });
+                );
             });
         }
     }
