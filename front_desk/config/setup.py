fa1893a99b3b437bea7b13093cf96e3c84d1f04e
anra54 <anugrah.ramadhani@gmail.com>
2019-09-10 17:47:42 +0700

    simplify after_install

--- front_desk/config/setup.py
+++ front_desk/config/setup.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, unicode_literals
 from frappe import _
 import frappe
+from frappe.desk.page.setup_wizard.setup_wizard import make_records
 
 
 def get_data():
@@ -45,53 +46,19 @@ def get_data():
 
 
 def after_install():
-	# setup Room Availability
-	room_availability_list = [
-		['AV', 'Available'],
-		['RS', 'Room Sold'],
-		['RC', 'Room Compliment'],
-		['HU', 'House Use'],
-		['OO', 'Out of Order'],
-		['OU', 'Office Use'],
-		['UC', 'Under Construction'],
+	records = [
+		# Room Availability
+		{'doctype': 'Room Availability', 'name':_('AV'), 'description': _('Available')},
+		{'doctype': 'Room Availability', 'name':_('RS'), 'description': _('Room Sold')},
+		{'doctype': 'Room Availability', 'name':_('RC'), 'description': _('Room Compliment')},
+		{'doctype': 'Room Availability', 'name':_('HU'), 'description': _('House Use')},
+		{'doctype': 'Room Availability', 'name':_('OO'), 'description': _('Out of Order')},
+		{'doctype': 'Room Availability', 'name':_('OU'), 'description': _('Office Use')},
+		{'doctype': 'Room Availability', 'name':_('UC'), 'description': _('Under Construction')},
+
+	# 	Hotel Tax
+		{'doctype': 'Hotel Tax', 'hotel_tax_description': _('None'), 'hotel_tex_value': 0},
+		{'doctype': 'Hotel Tax', 'hotel_tax_description': _('Tax & Service Include(21%)'), 'hotel_tex_value': 0},
+		{'doctype': 'Hotel Tax', 'hotel_tax_description': _('Tax Only(10%)'), 'hotel_tex_value': 0},
 	]
-	for ra in room_availability_list:
-		ra_entry = frappe.new_doc('Room Availability')
-		ra_entry.name = ra[0]
-		ra_entry.description = ra[1]
-		ra_entry.save()
-
-
-	# setup Hotel Tax
-	hotel_tax_list = [
-		['None', '0.0'],
-		['Tax & Service Include 21%', '21.0'],
-		['Tax Only 10%', '10.0'],
-
-	]
-
-	for ht in hotel_tax_list:
-		ha_entry = frappe.new_doc('Hotel Tax')
-		ha_entry.hotel_tax_description = ht[0]
-		# ha_entry.hotel_tax_value = ht[1]
-		ha_entry.hotel_tax_value = 0
-		ha_entry.save()
-
-	# # setup Chart of Accounts
-	# parent_account = frappe.db.get_list('Account', filters={'account_number': '1111.000'})[0].name
-	# account_list = [
-	# 	['Kas Front Office', '1111.003'],
-	# 	['Kas Kitchen', '1111.004'],
-	# 	['Kas F&B', '1111.005'],
-	# 	['Kas Housekeeping', '1111.006'],
-	# ]
-	#
-	# for ac in account_list:
-	# 	ac_entry = frappe.new_doc('Account')
-	# 	ac_entry.account_name = ac[0]
-	# 	ac_entry.account_number = ac[1]
-	# 	ac_entry.parent = parent_account
-	# 	ac_entry.parent_account = parent_account
-	# 	ac_entry.account_currency = 'IDR'
-	# 	ac_entry.account_type = 'Cash'
-	# 	ac_entry.save()
+	make_records(records, True)
