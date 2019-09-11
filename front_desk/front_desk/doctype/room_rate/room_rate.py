6f917c2bd2fe3f5bc2961c324b334e38cf90be8c
anra54 <anugrah.ramadhani@gmail.com>
2019-09-11 13:42:41 +0700

    room_rate terbagi jadi weekday dan weekend

--- front_desk/front_desk/doctype/room_rate/room_rate.py
+++ front_desk/front_desk/doctype/room_rate/room_rate.py
@@ -10,9 +10,20 @@ class RoomRate(Document):
 	pass
 
 def calculate_total_amount(doc, method):
-	total_rate = 0.0
+	total_rate_weekday = 0.0
+	total_rate_weekend = 0.0
+
+	# calculate weekday rate
 	for item in doc.get('room_rate_breakdown'):
 		sub_total = float(item.breakdown_qty) * float(item.breakdown_amount)
-		total_rate += sub_total
+		if (item.breakdown_name != 'Weekend Rate'):
+			total_rate_weekday += sub_total
 
-	doc.rate = total_rate
+	# calculate weekend rate
+	for item in doc.get('room_rate_breakdown'):
+		sub_total = float(item.breakdown_qty) * float(item.breakdown_amount)
+		if (item.breakdown_name != 'Weekday Rate'):
+			total_rate_weekend += sub_total
+
+	doc.rate_weekday = total_rate_weekday
+	doc.rate_weekend = total_rate_weekend
