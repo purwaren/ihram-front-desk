eed9ab5e9be50038c5477035368a1dbafb9f8b2e
anra54 <anugrah.ramadhani@gmail.com>
2019-09-16 17:44:39 +0700

    change hotel tax doctype

--- front_desk/front_desk/doctype/hotel_tax/hotel_tax.py
+++ front_desk/front_desk/doctype/hotel_tax/hotel_tax.py
@@ -7,4 +7,8 @@ import frappe
 from frappe.model.document import Document
 
 class HotelTax(Document):
-	pass
+	def autoname(self):
+		if self.hotel_tax_title:
+			company = frappe.get_doc("Global Defaults").default_company
+			abbr = frappe.get_cached_value('Company', company, 'abbr')
+			self.name = '{0} - {1}'.format(self.hotel_tax_title, abbr)
