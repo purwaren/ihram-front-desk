cdc6e87f5a6e2b440c49c9b68a0f805b396d309e
anra54 <anugrah.ramadhani@gmail.com>
2019-10-02 11:44:03 +0700

    hotel bill: calculate change only if mode of payment cash used

--- front_desk/front_desk/doctype/hotel_bill/hotel_bill.js
+++ front_desk/front_desk/doctype/hotel_bill/hotel_bill.js
@@ -4,6 +4,7 @@ var bill_breakdown = null;
 var breakdown_shown = false;
 var outstanding_now = 0;
 var deposit = 0;
+var cash_used_in_room_bill_payment = false;
 
 frappe.ui.form.on('Hotel Bill', {
 	onload: function(frm) {
@@ -39,6 +40,22 @@ frappe.ui.form.on('Hotel Bill', {
 });
 
 frappe.ui.form.on('Hotel Bill Payments', {
+	mode_of_payment: function (frm, cdt, cdn) {
+		let child = locals[cdt][cdn];
+		if (child.mode_of_payment == 'Cash') {
+			cash_used_in_room_bill_payment = true;
+			if (child.payment_amount > 0) {
+				var bp_list = frappe.get_doc( 'Hotel Bill', frm.doc.name ).bill_payments;
+				calculatePayments(frm, bp_list);
+			}
+		}
+		else {
+			cash_used_in_room_bill_payment = false;
+			frm.set_value('bill_change_amount', 0);
+			frm.set_value('bill_change_round_amount', 0);
+			frm.set_value('bill_rounded_change_amount', 0);
+		}
+	},
 	payment_amount: function (frm, cdt, cdn) {
 		var bp_list = frappe.get_doc( 'Hotel Bill', frm.doc.name ).bill_payments;
 		calculatePayments(frm, bp_list);
@@ -76,22 +93,26 @@ function calculatePayments(frm, bp_list) {
 	var diff = total_payment - frm.doc.bill_grand_total;
 	if (diff < 0) {
 		frm.set_value('bill_outstanding_amount', diff*-1);
-		frm.set_value('bill_change_amount', 0);
-		frm.set_value('bill_change_round_amount', 0);
-		frm.set_value('bill_rounded_change_amount', 0);
+		if (cash_used_in_room_bill_payment) {
+			frm.set_value('bill_change_amount', 0);
+			frm.set_value('bill_change_round_amount', 0);
+			frm.set_value('bill_rounded_change_amount', 0);
+		}
 	}
 	else {
 		frm.set_value('bill_outstanding_amount', 0);
-		frm.set_value('bill_change_amount', diff);
+		if (cash_used_in_room_bill_payment) {
+			frm.set_value('bill_change_amount', diff);
 
-		var roundedChange = Math.floor(diff / 100) * 100;
+			var roundedChange = Math.floor(diff / 100) * 100;
 
-		if (frm.doc.round_down_change == 1) {
-			frm.set_value('bill_change_round_amount', diff - roundedChange);
-			frm.set_value('bill_rounded_change_amount', roundedChange);
-		} else {
-			frm.set_value('bill_change_round_amount', 0);
-			frm.set_value('bill_rounded_change_amount', diff);
+			if (frm.doc.round_down_change == 1) {
+				frm.set_value('bill_change_round_amount', diff - roundedChange);
+				frm.set_value('bill_rounded_change_amount', roundedChange);
+			} else {
+				frm.set_value('bill_change_round_amount', 0);
+				frm.set_value('bill_rounded_change_amount', diff);
+			}
 		}
 	}
 }
