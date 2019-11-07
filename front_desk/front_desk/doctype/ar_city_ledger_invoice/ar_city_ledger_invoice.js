01f4ffe1cd86313c9322ac63185b84b7fe059052
anra54 <anugrah.ramadhani@gmail.com>
2019-11-07 13:13:47 +0700

    Doctype AR City Ledger Invoice

---
+++ front_desk/front_desk/doctype/ar_city_ledger_invoice/ar_city_ledger_invoice.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, PMM and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('AR City Ledger Invoice', {
+	refresh: function(frm) {
+
+	}
+});
