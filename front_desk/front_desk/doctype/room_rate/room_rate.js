0ec34abb1cd5e03f3ae389e67b4a82ee25d58421
anra54 <anugrah.ramadhani@gmail.com>
2019-11-29 17:06:48 +0700

    auto add room_rate_breakdown item weekday weekend

--- front_desk/front_desk/doctype/room_rate/room_rate.js
+++ front_desk/front_desk/doctype/room_rate/room_rate.js
@@ -3,5 +3,18 @@
 
 frappe.ui.form.on('Room Rate', {
 	refresh: function(frm, cdt, cdn) {
+		if (frm.doc.__islocal == 1) {
+			frm.set_value('room_rate_breakdown', []);
+
+		let weekend = frm.add_child('room_rate_breakdown');
+		weekend.breakdown_name = 'Weekend Rate';
+		weekend.breakdown_qty = 1;
+
+		let weekday = frm.add_child('room_rate_breakdown');
+		weekday.breakdown_name = 'Weekday Rate';
+		weekday.breakdown_qty = 1;
+
+		frm.refresh_field('room_rate_breakdown');
+		}
 	}
 });
