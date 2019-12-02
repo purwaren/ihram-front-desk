0c8cb6b617cff5b2468b65ed23ed7c02be5130e5
anra54 <anugrah.ramadhani@gmail.com>
2019-12-02 15:03:17 +0700

    add total rate after tax in room rate

--- front_desk/front_desk/doctype/room_rate/room_rate.py
+++ front_desk/front_desk/doctype/room_rate/room_rate.py
@@ -7,12 +7,17 @@ import frappe
 from frappe.model.document import Document
 from front_desk.front_desk.doctype.hotel_tax.hotel_tax import calculate_hotel_tax_and_charges
 
+
 class RoomRate(Document):
 	pass
+
 
 def calculate_total_amount(doc, method):
 	total_rate_weekday = 0.0
 	total_rate_weekend = 0.0
+	total_rate_weekday_after_tax = 0.0
+	total_rate_weekend_after_tax = 0.0
+	amount_multiplier = 1
 
 	# calculate weekday rate
 	for item in doc.get('room_rate_breakdown'):
@@ -26,19 +31,38 @@ def calculate_total_amount(doc, method):
 		if item.breakdown_name != 'Weekday Rate':
 			total_rate_weekend = total_rate_weekend + sub_total
 
+	for bd_item in doc.get('room_rate_breakdown'):
+		if bd_item.breakdown_name != 'Weekend Rate':
+			_, _, tb_total = calculate_hotel_tax_and_charges(
+				amount_multiplier * bd_item.breakdown_amount * float(bd_item.breakdown_qty), bd_item.breakdown_tax)
+			total_rate_weekday_after_tax = total_rate_weekday_after_tax + tb_total[-1]
+
+	for bd_item in doc.get('room_rate_breakdown'):
+		if bd_item.breakdown_name != 'Weekday Rate':
+			_, _, tb_total = calculate_hotel_tax_and_charges(
+				amount_multiplier * bd_item.breakdown_amount * float(bd_item.breakdown_qty), bd_item.breakdown_tax)
+			total_rate_weekend_after_tax = total_rate_weekend_after_tax + tb_total[-1]
+
 	doc.rate_weekday = total_rate_weekday
 	doc.rate_weekend = total_rate_weekend
+	doc.rate_weekday_after_tax = total_rate_weekday_after_tax
+	doc.rate_weekend_after_tax = total_rate_weekend_after_tax
 
-def populate_breakdown_summary(doc,method):
+
+def populate_breakdown_summary(doc, method):
 	summary = ""
 	breakdown_list = doc.get('room_rate_breakdown')
 	for index, item in enumerate(breakdown_list):
 		if item.breakdown_name == 'Weekend Rate' or item.breakdown_name == 'Weekday Rate':
-			summary = summary + str(index+1) + ". " + item.breakdown_name + ' : Rp.' + item.get_formatted("breakdown_amount") + " per night\n"
+			summary = summary + str(index + 1) + ". " + item.breakdown_name + ' : Rp.' + item.get_formatted(
+				"breakdown_amount") + " per night\n"
 		else:
-			summary = summary + str(index+1) + ". " + item.breakdown_qty + " " + item.breakdown_name + ' : Rp. ' + item.get_formatted("breakdown_amount") + " per pax\n"
+			summary = summary + str(
+				index + 1) + ". " + item.breakdown_qty + " " + item.breakdown_name + ' : Rp. ' + item.get_formatted(
+				"breakdown_amount") + " per pax\n"
 
 	doc.breakdown_summary = summary
+
 
 def get_rate_after_tax(room_rate_id, selector, discount):
 	if not discount:
@@ -47,12 +71,13 @@ def get_rate_after_tax(room_rate_id, selector, discount):
 	breakdown_list = room_rate.get('room_rate_breakdown')
 	total_weekday = 0.0
 	total_weekend = 0.0
-	amount_multiplier = 1 - float(discount)/100.0
+	amount_multiplier = 1 - float(discount) / 100.0
 
 	if selector == "Weekday Rate":
 		for bd_item in breakdown_list:
 			if bd_item.breakdown_name != 'Weekend Rate':
-				_,_,tb_total = calculate_hotel_tax_and_charges(amount_multiplier * bd_item.breakdown_amount * float(bd_item.breakdown_qty), bd_item.breakdown_tax)
+				_, _, tb_total = calculate_hotel_tax_and_charges(
+					amount_multiplier * bd_item.breakdown_amount * float(bd_item.breakdown_qty), bd_item.breakdown_tax)
 				total_weekday = total_weekday + tb_total[-1]
 
 		return total_weekday
@@ -60,7 +85,8 @@ def get_rate_after_tax(room_rate_id, selector, discount):
 	if selector == "Weekend Rate":
 		for bd_item in breakdown_list:
 			if bd_item.breakdown_name != 'Weekday Rate':
-				_,_,tb_total = calculate_hotel_tax_and_charges(amount_multiplier * bd_item.breakdown_amount * float(bd_item.breakdown_qty), bd_item.breakdown_tax)
+				_, _, tb_total = calculate_hotel_tax_and_charges(
+					amount_multiplier * bd_item.breakdown_amount * float(bd_item.breakdown_qty), bd_item.breakdown_tax)
 				total_weekend = total_weekend + tb_total[-1]
 
 		return total_weekend
