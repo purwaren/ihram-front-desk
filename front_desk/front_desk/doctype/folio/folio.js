f42d6f2d226337b0d3e2a31433263e559097a2d5
anra54 <anugrah.ramadhani@gmail.com>
2019-12-12 12:56:00 +0700

    print by type in folio

--- front_desk/front_desk/doctype/folio/folio.js
+++ front_desk/front_desk/doctype/folio/folio.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Folio', {
-	refresh: function(frm) {
+	print_type_selected: function(frm, cdt, cdn) {
+		let w = window.open(frappe.urllib.get_full_url("/printview?"
+				+"doctype="+encodeURIComponent("Folio")
+				+"&name="+encodeURIComponent(cdn)
+				+"&format="+encodeURIComponent("Print by Type")
+				+"&no_letterhead=0"
+				));
 
-	}
+			if (!w) {
+				frappe.msgprint(__("Please enable pop-ups")); return;
+			}
+
+	},
 });
