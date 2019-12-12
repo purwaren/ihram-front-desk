8f4286eebecedc0a86353c88c9583906ca0e618f
anra54 <anugrah.ramadhani@gmail.com>
2019-12-12 18:51:41 +0700

    prompt save if modified before print

--- front_desk/front_desk/doctype/folio/folio.js
+++ front_desk/front_desk/doctype/folio/folio.js
@@ -2,15 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Folio', {
+	onload: function (frm) {
+		frm.get_field("transaction_detail").grid.only_sortable();
+	},
 	print_type_selected: function(frm, cdt, cdn) {
-		let w = window.open(frappe.urllib.get_full_url("/printview?"
+		if (frm.doc.__unsaved) {
+			frappe.msgprint(__("Folio not saved, Please save the changes before printing."));
+		}
+		else {
+			let w = window.open(frappe.urllib.get_full_url("/printview?"
 				+"doctype="+encodeURIComponent("Folio")
 				+"&name="+encodeURIComponent(cdn)
 				+"&format="+encodeURIComponent("Print by Type")
 				+"&no_letterhead=0"
 				));
-		if (!w) {
-			frappe.msgprint(__("Please enable pop-ups")); return;
+			if (!w) {
+				frappe.msgprint(__("Please enable pop-ups")); return;
+			}
 		}
 	},
 });
