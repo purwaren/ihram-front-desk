63ff13829a376a898c4653bf98c79ee767249444
anra54 <anugrah.ramadhani@gmail.com>
2019-12-30 15:01:28 +0700

    Night Audit & Night Audit Transaction Doctype

---
+++ front_desk/front_desk/doctype/night_audit_transaction/night_audit_transaction.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, PMM and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Night Audit Transaction', {
+	refresh: function(frm) {
+
+	}
+});
