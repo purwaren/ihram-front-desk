311a5ac48ed6e4ca55c3b5d624fa29577e9be196
anra54 <anugrah.ramadhani@gmail.com>
2020-01-02 14:52:52 +0700

    tambah customer_id

--- front_desk/front_desk/doctype/night_audit/night_audit.py
+++ front_desk/front_desk/doctype/night_audit/night_audit.py
@@ -27,11 +27,11 @@ def fetch_transactions():
 	acli_list = frappe.get_all('AR City Ledger Invoice',
 							   filters={'creation': ['>=', midnight], 'change_amount': ['>', 0],
 										'change_journal_entry_id': ['=', '']},
-							   fields=['name', 'creation', 'rounded_change_amount'])
+							   fields=['name', 'creation', 'rounded_change_amount', 'customer_id'])
 	# 2. get all AR City Ledger Invoice Payment
 	aclip_list = frappe.get_all('AR City Ledger Invoice Payments',
 								filters={'creation': ['>=', midnight], 'journal_entry_id': ['=', '']},
-								fields=['name', 'creation', 'mode_of_payment', 'payment_amount'])
+								fields=['name', 'parent', 'creation', 'mode_of_payment', 'payment_amount'])
 	# 3. get all Hotel Bill Breakdown - Item and Tax
 	hbb_list = frappe.get_all('Hotel Bill Breakdown',
 								filters={'creation': ['>=', midnight], 'journal_entry_id': ['=', '']},
@@ -50,40 +50,115 @@ def fetch_transactions():
 	hb_depo_list = frappe.get_all('Hotel Bill',
 								  filters={'creation': ['>=', midnight], 'is_paid': ['=', 1], 'use_deposit': ['=', 1],
 										   'use_deposit_journal_entry_id': ['=', '']},
-								  fields=['name', 'creation', 'reservation_id', 'bill_deposit_amount'])
+								  fields=['name', 'creation', 'reservation_id', 'bill_deposit_amount', 'customer_id'])
 	# 7. get all Hotel BIll - Cash Change
 	hb_change_list = frappe.get_all('Hotel Bill',
 									filters={'creation': ['>=', midnight], 'is_paid': ['=', 1],
 											 'bill_change_amount': ['>', 0], 'change_journal_entry_id': ['=', '']},
-									fields=['name', 'creation', 'reservation_id', 'bill_rounded_change_amount'])
+									fields=['name', 'creation', 'reservation_id', 'bill_rounded_change_amount', 'customer_id'])
 	# 8. get all Folio Transaction
 	ft_list = frappe.get_all('Folio Transaction', filters={'creation': ['>=', midnight], 'journal_entry_id': ['=', '']})
 
-	if len(acli_list) > 0:
-		for acli_item in acli_list:
-			doc_nat = frappe.new_doc('Night Audit Transaction')
-			doc_nat.transaction_type = 'AR City Ledger Invoice - Change'
-			doc_nat.transaction_id = acli_item.name
-			doc_nat.amount = acli_item.rounded_change_amount
-			doc_nat.title = 'AR City Ledger Change: ' + acli_item.name
-			doc_nat.remark = doc_nat.title + ' -@' + str(acli_item.creation)
-			doc_nat.debit_account = frappe.db.get_list('Account', filters={'account_number': '1132.002'})[0].name
-			doc_nat.credit_account = frappe.db.get_list('Account', filters={'account_number': '1111.001'})[0].name
-			list_of_transactions.append(doc_nat)
+	# FOLIO TRANSACTIONS - START
+	if len(ft_list) > 0:
+		for ft_item in ft_list:
+			doc_ft = frappe.get_doc('Folio Transaction', ft_item.name)
+			customer_name = frappe.get_doc('Customer', frappe.get_doc('Reservation', frappe.db.get_value('Folio', {
+							'name': doc_ft.parent}, "reservation_id")).customer_id).name
+			if ' -  Additional Charge ' in doc_ft.remark:
+				doc_nat = frappe.new_doc('Night Audit Transaction')
+				doc_nat.transaction_type = 'Folio Transaction - Additional Charge'
+				doc_nat.transaction_id = ft_item.name
+				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
+				doc_nat.folio_id = doc_ft.parent
+				doc_nat.amount = doc_ft.amount
+				doc_nat.title = doc_ft.remark
+				doc_nat.remark = doc_ft.remark
+				doc_nat.debit_account = doc_ft.account_id
+				doc_nat.credit_account = doc_ft.against_account_id
+				doc_nat.customer_id = customer_name
+				list_of_transactions.append(doc_nat)
+			elif 'Early Check In Room ' in doc_ft.remark:
+				doc_nat = frappe.new_doc('Night Audit Transaction')
+				doc_nat.transaction_type = 'Folio Transaction - Room Stay Early Checkin'
+				doc_nat.transaction_id = ft_item.name
+				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
+				doc_nat.folio_id = doc_ft.parent
+				doc_nat.amount = doc_ft.amount
+				doc_nat.title = "JE " + doc_ft.name
+				doc_nat.remark = doc_ft.name + "-" + doc_ft.remark
+				doc_nat.debit_account = doc_ft.account_id
+				doc_nat.credit_account = doc_ft.against_account_id
+				doc_nat.customer_id = customer_name
+				list_of_transactions.append(doc_nat)
+			elif 'Late Check Out Room ' in doc_ft.remark:
+				doc_nat = frappe.new_doc('Night Audit Transaction')
+				doc_nat.transaction_type = 'Folio Transaction - Room Stay Late Checkout'
+				doc_nat.transaction_id = ft_item.name
+				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
+				doc_nat.folio_id = doc_ft.parent
+				doc_nat.amount = doc_ft.amount
+				doc_nat.title = "JE " + doc_ft.name
+				doc_nat.remark = doc_ft.name + "-" + doc_ft.remark
+				doc_nat.debit_account = doc_ft.account_id
+				doc_nat.credit_account = doc_ft.against_account_id
+				doc_nat.customer_id = customer_name
+				list_of_transactions.append(doc_nat)
+			elif 'Deposit ' in doc_ft.remark:
+				doc_nat = frappe.new_doc('Night Audit Transaction')
+				doc_nat.transaction_type = 'Folio Transaction - Reservation Deposit'
+				doc_nat.transaction_id = ft_item.name
+				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
+				doc_nat.folio_id = doc_ft.parent
+				doc_nat.amount = doc_ft.amount
+				doc_nat.title = "JE " + doc_ft.name
+				doc_nat.remark = doc_ft.remark
+				doc_nat.debit_account = doc_ft.account_id
+				doc_nat.credit_account = doc_ft.against_account_id
+				doc_nat.customer_id = customer_name
+				list_of_transactions.append(doc_nat)
+			elif 'Room Bill Payment: ' in doc_ft.remark:
+				doc_nat = frappe.new_doc('Night Audit Transaction')
+				doc_nat.transaction_type = 'Folio Transaction - Room Bill Payment'
+				doc_nat.transaction_id = ft_item.name
+				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
+				doc_nat.folio_id = doc_ft.parent
+				doc_nat.amount = doc_ft.amount
+				doc_nat.title = "JE " + doc_ft.name
+				doc_nat.remark = doc_ft.remark
+				doc_nat.debit_account = doc_ft.account_id
+				doc_nat.credit_account = doc_ft.against_account_id
+				doc_nat.customer_id = customer_name
+				list_of_transactions.append(doc_nat)
+			elif 'Change from ' in doc_ft.remark:
+				doc_nat = frappe.new_doc('Night Audit Transaction')
+				doc_nat.transaction_type = 'Folio Transaction - Room Bill Payment Change'
+				doc_nat.transaction_id = ft_item.name
+				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
+				doc_nat.folio_id = doc_ft.parent
+				doc_nat.amount = doc_ft.amount
+				doc_nat.title = "JE " + doc_ft.name
+				doc_nat.remark = doc_ft.remark
+				doc_nat.debit_account = doc_ft.account_id
+				doc_nat.credit_account = doc_ft.against_account_id
+				doc_nat.customer_id = customer_name
+				list_of_transactions.append(doc_nat)
+			elif 'Cancellation Fee - Reservation: ' in doc_ft.remark:
+				doc_nat = frappe.new_doc('Night Audit Transaction')
+				doc_nat.transaction_type = 'Folio Transaction - Reservation Cancellation Fee'
+				doc_nat.transaction_id = ft_item.name
+				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
+				doc_nat.folio_id = doc_ft.parent
+				doc_nat.amount = doc_ft.amount
+				doc_nat.title = doc_ft.name + ' ' + doc_ft.remark
+				doc_nat.remark = doc_ft.remark
+				doc_nat.debit_account = doc_ft.account_id
+				doc_nat.credit_account = doc_ft.against_account_id
+				doc_nat.customer_id = customer_name
+				list_of_transactions.append(doc_nat)
+	# FOLIO TRANSACTIONS - END
 
-	if len(aclip_list) > 0:
-		for aclip_item in aclip_list:
-			doc_nat = frappe.new_doc('Night Audit Transaction')
-			doc_nat.transaction_type = 'AR City Ledger Invoice Payments'
-			doc_nat.transaction_id = aclip_item.name
-			doc_nat.amount = aclip_item.payment_amount
-			doc_nat.title = 'AR City Ledger Payment (' + str(aclip_item.mode_of_payment) + '): ' + aclip_item.name
-			doc_nat.remark = doc_nat.title + ' -@' + str(aclip_item.creation)
-			doc_nat.debit_account = get_mode_of_payment_account(aclip_item.mode_of_payment,
-																frappe.get_doc("Global Defaults").default_company)
-			doc_nat.credit_account = frappe.db.get_list('Account', filters={'account_number': '1132.002'})[0].name
-			list_of_transactions.append(doc_nat)
-
+	# HOTEL BILL - START
 	if len(hbb_list) > 0:
 		for hbb_item in hbb_list:
 			doc_hbb = frappe.get_doc('Hotel Bill Breakdown', hbb_item.name)
@@ -97,6 +172,7 @@ def fetch_transactions():
 				doc_nat.remark = doc_nat.title + '. @ ' + str(hbb_item.creation)
 				doc_nat.debit_account = hbb_item.breakdown_account_against
 				doc_nat.credit_account = hbb_item.breakdown_account
+				doc_nat.customer_id = frappe.db.get_value('Hotel Bill', {'name': hbb_item.parent}, "customer_id")
 				list_of_transactions.append(doc_nat)
 			elif doc_hbb.is_excluded == 0 and doc_hbb.is_tax_item == 1:
 				doc_nat = frappe.new_doc('Night Audit Transaction')
@@ -108,6 +184,7 @@ def fetch_transactions():
 				doc_nat.remark = doc_nat.title + '. @ ' + str(hbb_item.creation)
 				doc_nat.debit_account = hbb_item.breakdown_account_against
 				doc_nat.credit_account = hbb_item.breakdown_account
+				doc_nat.customer_id = frappe.db.get_value('Hotel Bill', {'name': hbb_item.parent}, "customer_id")
 				list_of_transactions.append(doc_nat)
 
 	if len(hbr_list) > 0:
@@ -121,6 +198,7 @@ def fetch_transactions():
 			doc_nat.remark = doc_nat.title + '. @ ' + str(hbr_item.creation)
 			doc_nat.debit_account = hbr_item.account_against
 			doc_nat.credit_account = hbr_item.account
+			doc_nat.customer_id = frappe.db.get_value('Hotel Bill', {'name': hbb_item.parent}, "customer_id")
 			list_of_transactions.append(doc_nat)
 
 	if len(hbp_list) > 0:
@@ -136,6 +214,7 @@ def fetch_transactions():
 			doc_nat.debit_account = get_mode_of_payment_account(hbp_item.mode_of_payment,
 															 frappe.get_doc("Global Defaults").default_company)
 			doc_nat.credit_account = frappe.db.get_list('Account', filters={'account_number': '4320.001'})[0].name
+			doc_nat.customer_id = frappe.db.get_value('Hotel Bill', {'name': hbp_item.parent}, "customer_id")
 			list_of_transactions.append(doc_nat)
 
 	if len(hb_depo_list) > 0:
@@ -149,6 +228,7 @@ def fetch_transactions():
 			doc_nat.remark = doc_nat.title + ' - @' + str(hb_depo_item.creation)
 			doc_nat.debit_account = frappe.db.get_list('Account', filters={'account_number': '1172.000'})[0].name
 			doc_nat.credit_account = frappe.db.get_list('Account', filters={'account_number': '1132.001'})[0].name
+			doc_nat.customer_id = hb_depo_item.customer_id
 			list_of_transactions.append(doc_nat)
 
 	if len(hb_change_list) > 0:
@@ -162,95 +242,38 @@ def fetch_transactions():
 			doc_nat.remark = doc_nat.title + ' - @' + str(hb_change_item.creation)
 			doc_nat.debit_account = frappe.db.get_list('Account', filters={'account_number': '1132.001'})[0].name
 			doc_nat.credit_account = frappe.db.get_list('Account', filters={'account_number': '1111.001'})[0].name
+			doc_nat.customer_id = hb_change_item.customer_id
+			list_of_transactions.append(doc_nat)
+	# HOTEL BILL - END
+
+	# AR CITY LEDGER - START
+	if len(acli_list) > 0:
+		for acli_item in acli_list:
+			doc_nat = frappe.new_doc('Night Audit Transaction')
+			doc_nat.transaction_type = 'AR City Ledger Invoice - Change'
+			doc_nat.transaction_id = acli_item.name
+			doc_nat.amount = acli_item.rounded_change_amount
+			doc_nat.title = 'AR City Ledger Change: ' + acli_item.name
+			doc_nat.remark = doc_nat.title + ' -@' + str(acli_item.creation)
+			doc_nat.debit_account = frappe.db.get_list('Account', filters={'account_number': '1132.002'})[0].name
+			doc_nat.credit_account = frappe.db.get_list('Account', filters={'account_number': '1111.001'})[0].name
+			doc_nat.customer_id = acli_item.customer_id
 			list_of_transactions.append(doc_nat)
 
-	if len(ft_list) > 0:
-		for ft_item in ft_list:
-			doc_ft = frappe.get_doc('Folio Transaction', ft_item.name)
-			if ' -  Additional Charge ' in doc_ft.remark:
-				doc_nat = frappe.new_doc('Night Audit Transaction')
-				doc_nat.transaction_type = 'Folio Transaction - Additional Charge'
-				doc_nat.transaction_id = ft_item.name
-				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
-				doc_nat.folio_id = doc_ft.parent
-				doc_nat.amount = doc_ft.amount
-				doc_nat.title = doc_ft.remark
-				doc_nat.remark = doc_ft.remark
-				doc_nat.debit_account = doc_ft.account_id
-				doc_nat.credit_account = doc_ft.against_account_id
-				list_of_transactions.append(doc_nat)
-			elif 'Early Check In Room ' in doc_ft.remark:
-				doc_nat = frappe.new_doc('Night Audit Transaction')
-				doc_nat.transaction_type = 'Folio Transaction - Room Stay Early Checkin'
-				doc_nat.transaction_id = ft_item.name
-				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
-				doc_nat.folio_id = doc_ft.parent
-				doc_nat.amount = doc_ft.amount
-				doc_nat.title = "JE " + doc_ft.name
-				doc_nat.remark = doc_ft.name + "-" + doc_ft.remark
-				doc_nat.debit_account = doc_ft.account_id
-				doc_nat.credit_account = doc_ft.against_account_id
-				list_of_transactions.append(doc_nat)
-			elif 'Late Check Out Room ' in doc_ft.remark:
-				doc_nat = frappe.new_doc('Night Audit Transaction')
-				doc_nat.transaction_type = 'Folio Transaction - Room Stay Late Checkout'
-				doc_nat.transaction_id = ft_item.name
-				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
-				doc_nat.folio_id = doc_ft.parent
-				doc_nat.amount = doc_ft.amount
-				doc_nat.title = "JE " + doc_ft.name
-				doc_nat.remark = doc_ft.name + "-" + doc_ft.remark
-				doc_nat.debit_account = doc_ft.account_id
-				doc_nat.credit_account = doc_ft.against_account_id
-				list_of_transactions.append(doc_nat)
-			elif 'Deposit ' in doc_ft.remark:
-				doc_nat = frappe.new_doc('Night Audit Transaction')
-				doc_nat.transaction_type = 'Folio Transaction - Reservation Deposit'
-				doc_nat.transaction_id = ft_item.name
-				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
-				doc_nat.folio_id = doc_ft.parent
-				doc_nat.amount = doc_ft.amount
-				doc_nat.title = "JE " + doc_ft.name
-				doc_nat.remark = doc_ft.remark
-				doc_nat.debit_account = doc_ft.account_id
-				doc_nat.credit_account = doc_ft.against_account_id
-				list_of_transactions.append(doc_nat)
-			elif 'Room Bill Payment: ' in doc_ft.remark:
-				doc_nat = frappe.new_doc('Night Audit Transaction')
-				doc_nat.transaction_type = 'Folio Transaction - Room Bill Payment'
-				doc_nat.transaction_id = ft_item.name
-				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
-				doc_nat.folio_id = doc_ft.parent
-				doc_nat.amount = doc_ft.amount
-				doc_nat.title = "JE " + doc_ft.name
-				doc_nat.remark = doc_ft.remark
-				doc_nat.debit_account = doc_ft.account_id
-				doc_nat.credit_account = doc_ft.against_account_id
-				list_of_transactions.append(doc_nat)
-			elif 'Change from ' in doc_ft.remark:
-				doc_nat = frappe.new_doc('Night Audit Transaction')
-				doc_nat.transaction_type = 'Folio Transaction - Room Bill Payment Change'
-				doc_nat.transaction_id = ft_item.name
-				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
-				doc_nat.folio_id = doc_ft.parent
-				doc_nat.amount = doc_ft.amount
-				doc_nat.title = "JE " + doc_ft.name
-				doc_nat.remark = doc_ft.remark
-				doc_nat.debit_account = doc_ft.account_id
-				doc_nat.credit_account = doc_ft.against_account_id
-				list_of_transactions.append(doc_nat)
-			elif 'Cancellation Fee - Reservation: ' in doc_ft.remark:
-				doc_nat = frappe.new_doc('Night Audit Transaction')
-				doc_nat.transaction_type = 'Folio Transaction - Reservation Cancellation Fee'
-				doc_nat.transaction_id = ft_item.name
-				doc_nat.reservation_id = frappe.db.get_value('Folio', {'name': doc_ft.parent}, "reservation_id")
-				doc_nat.folio_id = doc_ft.parent
-				doc_nat.amount = doc_ft.amount
-				doc_nat.title = doc_ft.name + ' ' + doc_ft.remark
-				doc_nat.remark = doc_ft.remark
-				doc_nat.debit_account = doc_ft.account_id
-				doc_nat.credit_account = doc_ft.against_account_id
-				list_of_transactions.append(doc_nat)
+	if len(aclip_list) > 0:
+		for aclip_item in aclip_list:
+			doc_nat = frappe.new_doc('Night Audit Transaction')
+			doc_nat.transaction_type = 'AR City Ledger Invoice Payments'
+			doc_nat.transaction_id = aclip_item.name
+			doc_nat.amount = aclip_item.payment_amount
+			doc_nat.title = 'AR City Ledger Payment (' + str(aclip_item.mode_of_payment) + '): ' + aclip_item.name
+			doc_nat.remark = doc_nat.title + ' -@' + str(aclip_item.creation)
+			doc_nat.debit_account = get_mode_of_payment_account(aclip_item.mode_of_payment,
+																frappe.get_doc("Global Defaults").default_company)
+			doc_nat.credit_account = frappe.db.get_list('Account', filters={'account_number': '1132.002'})[0].name
+			doc_nat.customer_id = frappe.get_doc('AR City Ledger Invoice', aclip_item.parent).customer_id
+			list_of_transactions.append(doc_nat)
+	# AR CITY LEDGER - END
 
 	return list_of_transactions
 
