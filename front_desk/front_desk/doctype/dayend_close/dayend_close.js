3f8d920b7f4ee7aa7783f090e263d77527f3c5d1
anra54 <anugrah.ramadhani@gmail.com>
2020-02-07 16:49:35 +0700

    precheck dayend close: fetch all potentially problematic reservation and room stay

--- front_desk/front_desk/doctype/dayend_close/dayend_close.js
+++ front_desk/front_desk/doctype/dayend_close/dayend_close.js
@@ -3,6 +3,8 @@
 
 frappe.ui.form.on('Dayend Close', {
 	onload: function(frm) {
+		frm.get_field("dayend_reservation").grid.only_sortable();
+		frm.get_field("dayend_room_stay").grid.only_sortable();
 		if (frm.doc.__islocal == 1) {
 			frappe.call({
 				method: "front_desk.front_desk.doctype.dayend_close.dayend_close.is_there_open_dayend_close",
@@ -16,5 +18,35 @@ frappe.ui.form.on('Dayend Close', {
 				}
 			})
 		}
+	},
+	precheck_dayend_close: function (frm) {
+		frappe.call({
+			method: "front_desk.front_desk.doctype.dayend_close.dayend_close.precheck_dayend_close",
+			args: {
+				audit_date: frm.doc.audit_date,
+			},
+			callback: (r) => {
+				if (r.message) {
+					if (r.message[0].length > 0) {
+						frm.set_value('dayend_reservation',[]);
+						for (let i = 0; i < r.message[0].length; i++) {
+							let item = frm.add_child('dayend_reservation');
+							item.reservation_id = r.message[0][0]['reservation_id'];
+							item.description = r.message[0][0]['description'];
+						}
+					}
+					frm.refresh_field('dayend_reservation');
+					if (r.message[1].length > 0) {
+						frm.set_value('dayend_room_stay',[]);
+						for (let i = 0; i < r.message[1].length; i++) {
+							let item = frm.add_child('dayend_room_stay');
+							item.reservation_id = r.message[1][0]['room_stay_id'];
+							item.description = r.message[1][0]['description'];
+						}
+					}
+					frm.refresh_field('dayend_room_stay');
+				}
+			}
+		})
 	}
 });
--- front_desk/front_desk/doctype/dayend_close/dayend_close.py
+++ front_desk/front_desk/doctype/dayend_close/dayend_close.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe
+import datetime
 from frappe.model.document import Document
 
 class DayendClose(Document):
@@ -15,3 +16,39 @@ def is_there_open_dayend_close():
 		return 1
 	else:
 		return 2
+
+def fetch_problematic_reservation(audit_date):
+	reservation_to_returned = []
+
+	reservation_list = frappe.get_all('Reservation', {'status': 'Reserved'})
+	for reservation in reservation_list:
+		keys = ['reservation_id', 'description']
+		problematic_reservation = dict.fromkeys(keys, None)
+		reservation_detail_list = frappe.get_all('Reservation Detail', filters={'parent': reservation.name}, fields=['*'])
+		for rd_item in reservation_detail_list:
+			if rd_item.expected_arrival == audit_date:
+				problematic_reservation['reservation_id'] = reservation.name
+				problematic_reservation['description'] = 'Must Check In Today'
+				reservation_to_returned.append(problematic_reservation)
+
+	return reservation_to_returned
+
+def fetch_problematic_room_stay(audit_date):
+	room_stay_to_returned = []
+
+	room_stay_list = frappe.get_all('Room Stay', filters={'status': 'Checked In'}, fields=['*'])
+	for room_stay in room_stay_list:
+		keys = ['room_stay_id', 'description']
+		problematic_room_stay = dict.fromkeys(keys, None)
+		if room_stay.departure.date() == audit_date:
+			problematic_room_stay['room_stay_id'] = room_stay.name
+			problematic_room_stay['description'] = 'Must Check Out Today'
+			room_stay_to_returned.append(problematic_room_stay)
+
+	return  room_stay_to_returned
+
+@frappe.whitelist()
+def precheck_dayend_close(audit_date):
+	audit_date_object = datetime.datetime.strptime(audit_date, '%Y-%m-%d').date()
+	return fetch_problematic_reservation(audit_date_object), fetch_problematic_room_stay(audit_date_object)
+
